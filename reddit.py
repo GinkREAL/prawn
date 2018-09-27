ad43057bc0be3a6c8b2b909695fb204ef22efbe0
Kingsley <kingsleywong@rocketmail.com>
2018-09-28 06:08:06 +0800

    Switch over to prawn

--- reddit.py
+++ reddit.py
@@ -1,101 +1,140 @@
 from pymongo import MongoClient
-import json
+#import json
 import getpass
 import praw
-
+import datetime
 
 client = MongoClient('127.0.0.1', 27017)
 db = client.reddit
-rooturl = 'https://oauth.reddit.com/r/'
-headers = {'user-agent': 'script:kMJUL_qNcpDOpA:1.0 by /u/GinkREAL'}
-params = {'sort': 'best', 'raw_json': 1}
+errout = open('error.log', 'w')
 
-
-print("Please input username:", end='')
+print("Please input username: ", end='')
 username = input()
-password = getpass.getpass('Please input password:')
-accessrequest = requests.post('https://www.reddit.com/api/v1/access_token', headers={
-    'Authorization': 'Basic a01KVUxfcU5jcERPcEE6ajhaX2NIQ3NycFZUYjllZVhwQUxPTjR2Q3FJ',
-    'User-Agent': 'script:kMJUL_qNcpDOpA:1.0 by /u/GinkREAL'},
-    params={'grant_type': 'password', 'password': password, 'username': username})
-
-if(accessrequest.status_code == 200):
-    headers['Authorization'] = 'Bearer ' + accessrequest.json()['access_token']
-    print("Login successful. Token = " + accessrequest.json()['access_token'])
-else:
-    print("Error")
-    print(accessrequest.status_code)
-    print(accessrequest.json())
-    exit()
-
-
-def rprint(tree, indent):  # print with replies
-    print(("|---" * indent) + ">" + tree['data']['body'])
-    if tree['data']['replies'] == '':
-        return
-    else:
-        for branch in tree['data']['replies']['data']['children']:
-            if(branch['kind'] == 't1'):
-                rprint(branch, indent + 1)
+password = getpass.getpass('Please input password: ')
+reddit = praw.Reddit(client_id='kMJUL_qNcpDOpA',
+                     client_secret='j8Z_cHCsrpVTb9eeXpALON4vCqI',
+                     user_agent='script:kMJUL_qNcpDOpA:1.0 by /u/GinkREAL',
+                     username=username,
+                     password=password)
 
 
 def build(tree):
-    mapping = {'comment': tree['data']['body']}
-    debug_comments += 1
-    if tree['data']['replies'] == '':
-        return mapping
-    else:
-        replies = []
-        for branch in tree['data']['replies']['data']['children']:
-            if(branch['kind'] == 't1'):
-                replies.append(build(branch))
-                debug_comments += 1
+    mapping = {'comment': tree.body}
+    replies = []
+    subforest = tree.replies
+    subforest.replace_more(limit=None)
+    for branch in subforest:
+        replies.append(build(branch))
+    if len(replies) > 0:
         mapping['replies'] = replies
-        return mapping
+    return mapping
 
 
 # main code
-command = []
-subreddit = "/"
+command = ['notexit']
+subreddit = None
 while command[0] != "exit":
-    print(">", end='')
-    command = input().split()
-    if command[0] == 'sub':
-        print("Subreddit: " + subreddit)
-    elif command[0] == 'use':
-        print("Changed subreddit to: " + command[1])
-        subreddit = command[1]
-    elif command[0] == 'scrape':
-        debug_comments = 0
-        print("Requesting " + command[1])
-        data = requests.get(rooturl + subreddit + "/comments/" +
-                            command[1], headers=headers, params=params)
-        print("Done, storing in db.")
-        jdata = data.json()
-        mylist = []
-        for jsoncomment in jdata[1]['data']['children']:
-            if(jsoncomment['kind'] == 't1'):
-                mylist.append(build(jsoncomment))
-
-        db.comments.insert_one({'title': jdata[0]['data']['children']['data']['title'],
-                                'subreddit':  jdata[0]['data']['children']['data']['subreddit'],
-                                'name': jdata[0]['data']['children']['data']['name'],
-                                'upvote_ratio': jdata[0]['data']['children']['data']['upvote_ratio'],
-                                'domain': jdata[0]['data']['children']['data']['domain'],
-                                'author_fullanme': jdata[0]['data']['children']['data']['author_fullname'],
-                                'score': jdata[0]['data']['children']['data']['score'],
-                                'locked': jdata[0]['data']['children']['data']['locked'],
-                                'author': jdata[0]['data']['children']['data']['author'],
-                                'num_comments': jdata[0]['data']['children']['data']['num_comments'],
-                                'url': jdata[0]['data']['children']['data']['url'],
-                                'created_utc': jdata[0]['data']['children']['data']['created_utc'],
-
-                                'article_link': jdata[0]['data']['children']['data']['id'],
-                                'comments': mylist})
-        print("Done storing in db." + debug_comments)
-    elif command[0] == 'list':
-        print("Unimplemented command")
-    elif command[0] == 'autoscrape':
-        print("Unimplemented command")
+    if subreddit == None:
+        print("> ", end='')
     else:
-        print("Unknown command")
+        print(subreddit.display_name + "> ", end='')
+    command = input().split()
+    try:
+        if command[0] == 'use':  # change subreddit
+            print("Changed subreddit to: " + command[1])
+            subreddit = reddit.subreddit(command[1])
+        elif command[0] == 'scrape':  # scrape a specific article
+            submission = reddit.submission(id=command[1])
+
+            print("Doing: " + submission.title)
+            forest = submission.comments
+            forest.replace_more(limit=None)
+
+            allcomments = []
+            for tree in forest:
+                allcomments.append(build(tree))
+
+                db.comments.insert_one({'title': submission.title,
+                                        'subreddit':  submission.subreddit.display_name,
+                                        'name': submission.name,
+                                        'upvotes': submission.ups,
+                                        'downvotes': submission.downs,
+                                        'author_fullname': submission.author_fullname,
+                                        'score': submission.score,
+                                        'locked': submission.locked,
+                                        'author': submission.author.name,
+                                        'num_comments': submission.num_comments,
+                                        'url': submission.url,
+                                        'created_utc': submission.created_utc,
+                                        'article_id': submission.id,
+                                        'archived': submission.archived,
+                                        'comments': allcomments})
+        elif command[0] == 'hot':  # scrapes a set amount of articles from hot
+            print("TOP OF /r/" + subreddit.display_name)
+            for submission in subreddit.top(limit=10):
+                print("> " + submission.title)
+        elif command[0] == 'auto':  # scrapes a set amount of articles from top
+            for submission in subreddit.top(limit=200):
+                try:
+                    print("Doing: " + submission.title)
+
+                    existingcopy = db.comments.find_one(
+                        {'article_id': submission.id})
+                    if existingcopy != None:
+                        if existingcopy['archived']:
+                            print(" Skipping due to being archived in db")
+                            continue
+                        print(" Updating existing record")
+                        db.comments.delete_one({'_id': existingcopy['_id']})
+
+                    print(" Loading comments (this may take awhile)")
+                    forest = submission.comments
+                    print(" Populating forest", end='')
+                    while True:
+                        try:
+                            forest.replace_more(limit=50)
+                            print("")
+                            break
+                        except PossibleExceptions:
+                            print(".", end='')
+                            usleep(500)
+                    print(" Done, finalizing insertion")
+
+                    allcomments = []
+                    for tree in forest:
+                        allcomments.append(build(tree))
+
+                    db.comments.insert_one({'title': submission.title,
+                                            'subreddit':  subreddit.display_name,
+                                            'name': submission.name,
+                                            'upvotes': submission.ups,
+                                            'downvotes': submission.downs,
+                                            'author_fullname': submission.author_fullname,
+                                            'score': submission.score,
+                                            'locked': submission.locked,
+                                            'author': submission.author.name,
+                                            'num_comments': submission.num_comments,
+                                            'url': submission.url,
+                                            'created_utc': submission.created_utc,
+                                            'article_id': submission.id,
+                                            'archived': submission.archived,
+                                            'comments': allcomments})
+                    print("Done")
+                except Exception as e:
+                    print("Error in " + submission.title)
+                    print("Error in " + submission.title, file=errout)
+                    print(e, file=errout)
+                    errout.flush()
+                    continue
+            print("AUTO COMPLETE AT", end='')
+            print(datetime.datetime.now())
+        elif command[0] == 'touchall':  # dont use, updates all modified timestamps
+            print("Unimplemented function")
+        elif command[0] == 'forceupdate':  # updates all non archived rgardless of timestamp
+            print("Unimplemented function")
+        else:
+            print("Unknown command")
+    except Exception as e:
+        print("===ERROR===")
+        print(e)
+        continue
