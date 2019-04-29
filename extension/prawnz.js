73234571bc751ba48f3f682d79f9ba9c6d3be2e5
Kingsley <kingsleywong@rocketmail.com>
2019-04-29 13:47:56 +0800

    Update extension

--- extension/prawnz.js
+++ extension/prawnz.js
@@ -1,56 +1,91 @@
+baseURL = "http://localhost:8080/ai/article" //dev
+//baseURL = "https://prawn.ml/ai/article" //prod
 
+var pageurl = document.URL.split("/")[6];
 
-//element = document.getElementsByClassName("qtb4sn-2 iOpWuy")[0]
-element = document.getElementsByClassName("RffBqG1xCvuy1r49wYhLr")[0]
-// element2 = document.getElementsByClassName("rhbaa2-2 ggsIRT")[0]
-viz = '<div class="s17ivpdx-0 jAbabm" style="margin-bottom:20px"><div class="_ZhON3a3vplThB8NFwuJn"><div class="_2sggAEfRQLyoAl4J__5twU">Commenters\' Thoughts</div></div><canvas id="stackedBar"></canvas><div class="qcxaFzxDBtnkGs5hS18OZ" style="text-align:right; color:rgb(124, 124, 124); margin:10px;">Powered by PRAWn</div></div>'
-element.insertAdjacentHTML('afterbegin',viz);
+const loadChart = (j) => {
+    element = document.getElementsByClassName("RffBqG1xCvuy1r49wYhLr")[0] //regular
+    if(element == null) element = document.getElementsByClassName("s1gwtmx8-2")[0] //overlay
+    // div list
+    //s1km3tr3-2 cqEWZA
+    //s1gwtmx8-2
+    //RffBqG1xCvuy1r49wYhLr s1gwtmx8-2 KpCIH
+    viz = '<div class="s17ivpdx-0 jAbabm" style="margin-bottom:20px"><div class="_ZhON3a3vplThB8NFwuJn"><div class="_2sggAEfRQLyoAl4J__5twU">Commenters\' Thoughts</div></div><canvas id="stackedBar"></canvas><div class="qcxaFzxDBtnkGs5hS18OZ" style="text-align:right; color:rgb(124, 124, 124); margin:10px;">Powered by PRAWn</div></div>'
+    element.insertAdjacentHTML('afterbegin',viz);
 
-const articlestring = "76rjtv"
-console.log("ey")
-fetch('http://localhost:8080/ai/article?article=76rjtv')
+    dataStacked = {
+        labels: ["Target"],
+        datasets: [{
+            type: 'horizontalBar',
+            label: 'In Favor',
+            data: [j.commentsFavoring],
+            backgroundColor: 'rgba(54, 162, 235, 1)'
+        }, {
+            type: 'horizontalBar',
+            label: 'Against',
+            data: [j.commentsAgainst],
+            backgroundColor: 'rgba(255, 99, 132, 1)'
+        }, {
+            type: 'horizontalBar',
+            label: 'None',
+            data: [j.commentsNeutral],
+            backgroundColor: 'rgba(255, 205, 86, 1)'
+        }]
+    };
+
+    var ctx = document.getElementById('stackedBar').getContext('2d');
+
+    var stackedBar = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: dataStacked,
+        options: {
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    });
+}
+
+async function main() {
+    console.log("PRAWN Starting")
+    var response = await fetch(baseURL + "?article=" + pageurl)
     .then((response) => {
-        return response.json()
-    }, (error) => {
+        return response
+    })
+    .catch((error) => {
         console.log(error)
     })
-    .then((j) => {
-        console.log(j)
 
-        dataStacked = {
-            labels: ["Target"],
-            datasets: [{
-                type: 'horizontalBar',
-                label: 'In Favor',
-                data: [j.commentsFavoring],
-                backgroundColor: 'rgba(54, 162, 235, 1)'
-            }, {
-                type: 'horizontalBar',
-                label: 'Against',
-                data: [j.commentsAgainst],
-                backgroundColor: 'rgba(255, 99, 132, 1)'
-            }, {
-                type: 'horizontalBar',
-                label: 'None',
-                data: [j.commentsNeutral],
-                backgroundColor: 'rgba(255, 205, 86, 1)'
-            }]
-        };
+    console.log(response.status)
+    if(response.status == 200){
+        console.log("Status:200")
+        loadChart(await response.json())
+    } else if (response.status == 404){
+        console.log("Status:404")
+        var bd = new FormData()
+        bd.append('article', pageurl)
+        fetch(baseURL, {
+            method: "POST",
+            body: bd
+        })
+        .then((response) => {
+            console.log("Request")
+            console.log(JSON.stringify(bd))
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log("Request error")
+        })
+        setTimeout(main, 7000)
+    } else if (response.status == 208){
+        console.log("Status:208")
+        setTimeout(main, 7000)
+    }
+}
 
-        var ctx = document.getElementById('stackedBar').getContext('2d');
-
-        var stackedBar = new Chart(ctx, {
-            type: 'horizontalBar',
-            data: dataStacked,
-            options: {
-                scales: {
-                    xAxes: [{
-                        stacked: true
-                    }],
-                    yAxes: [{
-                        stacked: true
-                    }]
-                }
-            }
-        });
-    })
+main()
